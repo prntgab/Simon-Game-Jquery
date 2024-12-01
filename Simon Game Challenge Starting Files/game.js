24e0b2e92a9ef2fae969a3e36de27a4b4f916888
Prnt_Apple <125944479+prntgab@users.noreply.github.com>
2024-12-01 13:40:55 +0800

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,18 +1,30 @@
-let greenBtn = $('#green').click(function(){
-    alert("yooo");
-});
+$(document).ready(function(){
+    let greenBtn = $('#green').click(function(){
+        alert("yooo");
+        return 3;
+    });
 
-let redBtn = $('#red').click(function(){
-    alert("yooo");
-});
+     $('#red').click(function(){
+        return 1;
+    });
+    let redBtn =    $('.red').css({
+        "-webkit-box-shadow": "inset -1px -1px 99px 25px #FFFFFF", 
+        "box-shadow": "inset -1px -1px 99px 25px #FFFFFF"
+    });
+    
+    let blueBtn = $('#blue').click(function(){
+        alert("yooo");
+        return 2;
+    });
 
-let blueBtn = $('#blue').click(function(){
-    alert("yooo");
+    let yellowBtn = $('#yellow').click(function(){
+        const matchedColors = findColor(array, buttonColor); 
+        return 4;
+       
+    });
 });
-
-let yellowBtn = $('#yellow').click(function(){
-    alert("yooo");
-});
+//yellow = start
+//blue next level
 
 let randomNumber = () => Math.floor(Math.random() * 4) + 1;
 
@@ -24,21 +36,53 @@ const buttonColor ={
 };
 
 
-const levelUp = () =>{
-    
+
+let levelUp = () =>{
+    let arrColor = [];
+    for(let i = 0; i < 4; i++){ 
+        randomNumber();
+        arrColor.push(randomNumber())
+    }
+    return arrColor;
 }
 
+let array = levelUp();
 
-
-const compareColor = () => {
-    const randomNum = randomNumber();
-    let colorMatch = 0;
-
-    for(let color in buttonColor){
-        if(buttonColor[color] === randNum){
-            colorMatch = color;
-            break;
+let findColor = (arr, obj) => {
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        for(const key in obj){
+            if(obj[key] === arr[i]){
+                result.push(key)
+            }
         }
     }
-    return color;
+    for (const key in obj)
+        $(`#${key}`).css("color", "#FFFFFF"); //this where i finsih
+    console.log(result);
+    return result;
 }
+
+let simonSaysDelay = async (arr) => {
+    for(let i = 0; i < arr.length; i++ ){
+       
+    }
+}
+
+
+
+
+
+
+// const compareColor = () => {
+//     const randomNum = randomNumber();
+//     let colorMatch = 0;
+
+//     for(let color in buttonColor){
+//         if(buttonColor[color] === randNum){
+//             colorMatch = color;
+//             break;
+//         }
+//     }
+//     return color;
+// }
